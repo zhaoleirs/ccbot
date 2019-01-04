da0a2d667d08a0f1321a1fc1b1ac09d7d3aba9cc
zhaolei <401525605@qq.com>
2019-01-04 10:44:21 +0800

    modify

--- ThadHack/Engines/Grind/States/PartyStates/partyStateFindTarget.cs
+++ ThadHack/Engines/Grind/States/PartyStates/partyStateFindTarget.cs
@@ -5,7 +5,7 @@ namespace ZzukBot.Engines.Grind.States
     internal class PartyStateFindTarget : StateFindTarget
     {
 
-        internal override bool NeedToRun => PartyAssist.Local.isLeader && PartyAssist.InSignAll() && base.NeedToRun;
+        internal override bool NeedToRun => PartyAssist.Local.isLeader && PartyAssist.InSignAll() &&!PartyAssist.Resting && base.NeedToRun;
 
     }
 }
